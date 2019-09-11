3fe646bb3e96e8a29fd255a13df684bbd8f2cdce
Mark02Lopez <mamorales@todoensubastas.com.mx>
2019-09-11 09:59:55 -0500

    se agrego en el archivo db, schema, y modelo en resolvers se aagregaron mutaation como queries especialmente en Productos

--- data/db.js
+++ data/db.js
@@ -2,20 +2,31 @@ import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/clientes', {userNewUrlPassword: true});
+mongoose.connect('mongodb://localhost/clientes', { userNewUrlPassword: true });
+
+mongoose.set('useFindAndModify', false);
 
 /// definir el schema de clientes para la bd 
 
-const clientesSchema = new mongoose.Schema({ 
+const clientesSchema = new mongoose.Schema({
     nombre: String,
     apellidos: String,
     empresa: String,
     emails: Array,
     edad: Number,
     tipo: String,
-    pedidos: Array 
+    pedidos: Array
 });
 
-const Clientes =  mongoose.model('clientes', clientesSchema);
+const Clientes = mongoose.model('clientes', clientesSchema);
 
-export { Clientes };
+/// eschema para los productos y se aagregará a la BD
+const porductosSchema = new mongoose.Schema({
+    nombre: String,
+    precio: Number,
+    stock: Number
+});
+
+const Productos = mongoose.model('productos', porductosSchema);
+
+export { Clientes, Productos };
--- data/resolvers.js
+++ data/resolvers.js
@@ -1,74 +1,139 @@
 // import mongose from 'mongose';
-import { Clientes } from './db';
+import { Clientes, Productos } from './db';
 import { rejects } from 'assert';
 
- export const resolvers = { 
-     Query: {
-        getClientes: (root ,{limite}) => { 
-            return new Promise((resolve, object) => { 
-                Clientes.find({}, (error, clientes) => { 
-                    if(error) rejects(error)
+export const resolvers = { 
+    Query: {
+        getClientes: (root, { offset, limite }) => {
+            return new Promise((resolve, object) => {
+                Clientes.find({}, (error, clientes) => {
+                    if (error) rejects(error)
                     else resolve(clientes)
-                }).limit(limite) 
+                }).limit(limite).skip(offset)
             })
         },
-        // getClientes : (root, {limite}) => { 
-        //     return new Clientes.find({}).limit(limite)
-        // },
-
-        // getCliente: ({id}) => {
-        //     return new Cliente(id, clientesDB[id]);
-        // },
-        getCliente: (root, {id}) => { 
-            return new Promise((resolve, object) => { 
+        getCliente: (root, { id }) => {
+            return new Promise((resolve, object) => {
                 Clientes.findById(id, (error, clientes) => {
-                    if(error) rejects(error)
+                    if (error) rejects(error)
                     else resolve(clientes)
+                })
+            })
+        },
+        totalClientes: (root) => {
+            return new Promise((resolve, object) => {
+                Clientes.countDocuments({}, (error, count) => {
+                    // ssi existe un error maandar el metodo de rechazo y como paraametro el
+                    if (error) rejects(error)
+                    else resolve(count)
+                })
+            })
+        },
+        // obteniendo los productos
+        obtainProducts: (root, { offset, limite }) => {
+            return new Promise((resolve, object) => {
+                Productos.find({}, (error, productos) => {
+                    if (error) rejects(error);
+                    else resolve(productos)
+                }).limit(limite).skip(offset)
+            })
+        },
+        getProduct: (root, { id }) => {
+            return new Promise((resolve, rejects) => {
+                Productos.findById(id, (error, producto) => {
+                    if (error) rejects(error);
+                    else resolve(producto)
+                })
+            })
+        },
+        totalProducts: (root) => {
+            return new Promise((resolve, object) => {
+                Productos.countDocuments({}, (error, count) => {
+                    // ssi existe un error maandar el metodo de rechazo y como paraametro el
+                    if (error) rejects(error)
+                    else resolve(count)
                 })
             })
         }
-     },
-     Mutation: { 
-         crearCliente : (root, {input}) => {
-            const nuevoCliente = new Clientes({ 
-                nombre : input.nombre,
-                apellidos : input.apellidos,
-                empresa : input.empresa,
-                emails : input.emails,
-                edad :  input.edad,
-                tipo : input.tipo,
-                pedidos : input.pedidos
-            });            
+    },
+    Mutation: {
+        crearCliente: (root, { input }) => {
+            const nuevoCliente = new Clientes({
+                nombre: input.nombre,
+                apellidos: input.apellidos,
+                empresa: input.empresa,
+                emails: input.emails,
+                edad: input.edad,
+                tipo: input.tipo,
+                pedidos: input.pedidos
+            });
 
             // agregar el id 
-            nuevoCliente.id =  nuevoCliente._id;
+            nuevoCliente.id = nuevoCliente._id;
 
             //& agregarlo a base de datos
-            return new Promise((resolve, object) => { 
-                nuevoCliente.save((err) => { 
-                    if(err) rejects(err)
+            return new Promise((resolve, object) => {
+                nuevoCliente.save((err) => {
+                    if (err) rejects(err)
                     else resolve(nuevoCliente)
                 })
             })
-         },
+        },
 
-         // mutation para la actualizacion 
-         actualizarCliente: (root, {input}) => { 
-             return new Promise((resolve, object) => { 
-                 Clientes.findOneAndUpdate({ _id: input.id}, input, {new: true}, (error, cliente) => { 
-                     if(error) rejects(error)
-                     else resolve(cliente)
-                 })
-             })
-         },         
-         /// eliminar clientes por medio de id
-         eliminarCliente: ( root, {id}) => { 
-            return new Promise((resolve, object) => { 
-                Clientes.deleteOne({_id: id}, (error) => { 
-                    if(error) rejects(error)
+        // mutation para la actualizacion 
+        actualizarCliente: (root, { input }) => {
+            return new Promise((resolve, object) => {
+                Clientes.findOneAndUpdate({ _id: input.id }, input, { new: true }, (error, cliente) => {
+                    if (error) rejects(error)
+                    else resolve(cliente)
+                })
+            })
+        },
+        /// eliminar clientes por medio de id
+        eliminarCliente: (root, { id }) => {
+            return new Promise((resolve, object) => {
+                Clientes.deleteOne({ _id: id }, (error) => {
+                    if (error) rejects(error)
                     else resolve("Se elimino correctamente el cliente")
                 })
             })
-         }
-     }
- }
+        },
+        // nuevos Productos  
+        nuevoProducto: (root, { input }) => {
+            //creaaamos el schemaa para la bd 
+            const nuevoProducto = new Productos({
+                nombre: input.nombre,
+                precio: input.precio,
+                stock: input.stock
+            });
+
+            //agregamos el id  que se asigna al objeto 
+            nuevoProducto.id = nuevoProducto._id;
+
+            /// agregar a la BD 
+            return new Promise((resolve, object) => {
+                nuevoProducto.save((error) => {
+                    if (error) rejects(error);
+                    else resolve(nuevoProducto)
+                })
+            })
+        },
+        updateProduct: (root, { input }) => {
+            return new Promise((resolve, object) => {
+                Productos.findOneAndUpdate({ _id: input.id }, input, { new: true }, (error, producto) => {
+                    if (error) rejects(error)
+                    else resolve(producto)
+                })
+            })
+        },
+        deleteProduct: (root, { id }) => {
+            return new Promise((resolve, object) => {
+                // accion de mongosse 
+                Productos.deleteOne({ _id: id }, (err) => {
+                    if (err) rejects(err);
+                    else resolve('Se elimino correctamente producto');
+                })
+            })
+        },
+    }
+}
